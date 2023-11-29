9c54f6d2733f9af4b091479eb14dd38c0f52be3e
kassra niroumand <kassra.niroumand@gmail.com>
2023-11-29 13:29:54 +0000

    Develop ProductClient and UserClient Implementations

---
+++ order-service/src/main/java/com/example/orderservice/client/ProductClient.java
@@ -0,0 +1,33 @@
+package com.example.orderservice.client;
+
+import com.example.orderservice.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+/*
+* Contain method to send data to product service
+* */
+@Service
+public class ProductClient {
+    private final WebClient webClient;
+
+    public ProductClient(@Value("${product.service.url}") String url)
+    {
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(url)
+                .build();
+    }
+
+    public Mono<ProductDto> getProductById(final String productId){
+        return this.webClient
+                .get()
+                .uri("{id}",productId)
+                .retrieve()
+                .bodyToMono(ProductDto.class);
+    }
+
+
+}
---
+++ order-service/src/main/java/com/example/orderservice/client/UserClient.java
@@ -0,0 +1,37 @@
+package com.example.orderservice.client;
+
+import com.example.orderservice.dto.ProductDto;
+import com.example.orderservice.dto.TransactionRequestDto;
+import com.example.orderservice.dto.TransactionResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+/**
+* Contain method to send data to user service
+**/
+@Service
+public class UserClient {
+    private final WebClient webClient;
+
+    public UserClient(@Value("${user.service.url}") String url)
+    {
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(url)
+                .build();
+    }
+
+
+    public Mono<TransactionResponseDto> authorizeTransaction(final TransactionRequestDto transactionRequestDto){
+        return this.webClient
+                .post()
+                .uri("transaction")
+                .bodyValue(transactionRequestDto)
+                .retrieve()
+                .bodyToMono(TransactionResponseDto.class);
+    }
+
+
+}
